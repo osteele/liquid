fc6d96ed408c42a93949f05fccb8be56a8b75a15
Oliver Steele <steele@osteele.com>
2017-06-27 13:18:11 -0400

    Chunk scanner test cases

---
+++ chunks/scanner_test.go
@@ -0,0 +1,45 @@
+package chunks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var scannerTests = []struct{ in, expected string }{
+	{"{{12}}", "12"},
+}
+
+func TestScanner(t *testing.T) {
+	tokens := Scan("12", "")
+	require.NotNil(t, tokens)
+	require.Len(t, tokens, 1)
+	require.Equal(t, TextChunkType, tokens[0].Type)
+	require.Equal(t, "12", tokens[0].Source)
+
+	tokens = Scan("{{obj}}", "")
+	require.NotNil(t, tokens)
+	require.Len(t, tokens, 1)
+	require.Equal(t, ObjChunkType, tokens[0].Type)
+	require.Equal(t, "obj", tokens[0].Args)
+
+	tokens = Scan("{{ obj }}", "")
+	require.NotNil(t, tokens)
+	require.Len(t, tokens, 1)
+	require.Equal(t, ObjChunkType, tokens[0].Type)
+	require.Equal(t, "obj", tokens[0].Args)
+
+	tokens = Scan("{%tag args%}", "")
+	require.NotNil(t, tokens)
+	require.Len(t, tokens, 1)
+	require.Equal(t, TagChunkType, tokens[0].Type)
+	require.Equal(t, "tag", tokens[0].Tag)
+	require.Equal(t, "args", tokens[0].Args)
+
+	tokens = Scan("{% tag args %}", "")
+	require.NotNil(t, tokens)
+	require.Len(t, tokens, 1)
+	require.Equal(t, TagChunkType, tokens[0].Type)
+	require.Equal(t, "tag", tokens[0].Tag)
+	require.Equal(t, "args", tokens[0].Args)
+}
