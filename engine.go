ebc29dcbed605a1e2094c7f7a05f03cb0474b1a1
Oliver Steele <steele@osteele.com>
2017-06-30 11:20:32 -0400

    Separate interface.go from engine.go

--- engine.go
+++ engine.go
@@ -1,8 +1,3 @@
-/*
-Package liquid is a pure Go implementation of Shopify Liquid templates, for use in https://github.com/osteele/gojekyll.
-
-See the project README https://github.com/osteele/liquid for additional information and implementation status.
-*/
 package liquid
 
 import (
@@ -20,42 +15,6 @@ func init() {
 	tags.DefineStandardTags()
 	filters.DefineStandardFilters()
 }
-
-// Engine parses template source into renderable text.
-//
-// An engine can be configured with additional filters and tags.
-//
-// Filters
-//
-// DefineFilter defines a Liquid filter.
-//
-// A filter is any function that takes at least one input, and returns one or two outputs.
-// If it returns two outputs, the second must be an error.
-type Engine interface {
-	// DefineFilter defines a filter function e.g. {{ value | filter: arg }}.
-	//
-	// Note: Although this function is defined on the engine, its effect is currently global.
-	DefineFilter(name string, fn interface{})
-	// DefineTag defines a tag function e.g. {% tag %}.
-	//
-	// Note: Although this function is defined on the engine, its effect is currently global.
-	DefineTag(string, TagDefinition)
-	// Note: Although this function is defined on the engine, its effect is currently global.
-	DefineStartTag(string, TagDefinition)
-
-	ParseTemplate(b []byte) (Template, error)
-	// ParseAndRender parses and then renders the template.
-	ParseAndRender(b []byte, bindings map[string]interface{}) ([]byte, error)
-	// ParseAndRenderString is a convenience wrapper for ParseAndRender, that has string input and output.
-	ParseAndRenderString(s string, bindings map[string]interface{}) (string, error)
-}
-
-// Renderer is the type of a function that is evaluated within a context and writes to output.
-type Renderer func(io.Writer, chunks.Context) error
-
-// TagDefinition is the type of a function that parses the argument string "args" from a tag "{% tagname args %}",
-// and returns a renderer.
-type TagDefinition func(parameters string) (func(io.Writer, chunks.Context) error, error)
 
 type engine struct{}
 
