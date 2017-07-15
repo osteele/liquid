724da617c29933e2a4bf2308faeeff72685ebd5a
Oliver Steele <steele@osteele.com>
2017-07-15 13:29:53 -0400

    Remove IsTemplateError

--- liquid.go
+++ liquid.go
@@ -9,9 +9,6 @@ The liquid package itself is versioned in gopkg.in. Subpackages have no compatib
 package liquid
 
 import (
-	"github.com/osteele/liquid/evaluator"
-	"github.com/osteele/liquid/expressions"
-	"github.com/osteele/liquid/parser"
 	"github.com/osteele/liquid/render"
 )
 
@@ -32,30 +29,4 @@ type SourceError interface {
 	Cause() error
 	Path() string
 	LineNumber() int
-}
-
-// IsTemplateError returns true iff the error represents an error in the template
-// syntax or execution. It is used to distinguish errors in input values from errors in the Liquid implementation, or the implementation of tags and filters, themselves.
-//
-// Use this function to avoid coding the specific types of subpackage errors, which
-// are likely to change.
-func IsTemplateError(err error) bool {
-	switch err.(type) {
-	// TODO some of these clauses, or maybe the entire function, is unnecessary
-	// now that interface calls return SourceError
-	case evaluator.TypeError:
-		return true
-	case expressions.InterpreterError:
-		return true
-	case expressions.ParseError:
-		return true
-	case parser.Error:
-		return true
-	case render.Error:
-		return true
-	case SourceError:
-		return true
-	default:
-		return false
-	}
 }
--- liquid_test.go
+++ liquid_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestIsTemplateError(t *testing.T) {
 	_, err := NewEngine().ParseAndRenderString("{{ syntax error }}", emptyBindings)
-	require.True(t, IsTemplateError(err))
+	require.Error(t, err)
 	_, err = NewEngine().ParseAndRenderString("{% if %}", emptyBindings)
-	require.True(t, IsTemplateError(err))
+	require.Error(t, err)
 	_, err = NewEngine().ParseAndRenderString("{% unknown_tag %}", emptyBindings)
-	require.True(t, IsTemplateError(err))
+	require.Error(t, err)
 	_, err = NewEngine().ParseAndRenderString("{% a | unknown_filter %}", emptyBindings)
-	require.True(t, IsTemplateError(err))
+	require.Error(t, err)
 }
