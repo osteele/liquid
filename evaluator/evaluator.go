fb70314da4ffcb6715200ea07d32ccef221c63ad
Oliver Steele <steele@osteele.com>
2017-07-27 18:28:16 -0400

    Add forwarders from evaluator pkg

---
+++ evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"reflect"
+	"time"
+
+	"github.com/osteele/liquid/values"
+)
+
+// Convert should be replaced by values.Convert.
+func Convert(value interface{}, typ reflect.Type) (interface{}, error) {
+	return values.Convert(value, typ)
+}
+
+// MustConvertItem should be replaced by values.Convert.
+func MustConvertItem(item interface{}, array interface{}) interface{} {
+	return values.MustConvertItem(item, array)
+}
+
+// Sort should be replaced by values.
+func Sort(data []interface{}) {
+	values.Sort(data)
+}
+
+// SortByProperty should be replaced by values.SortByProperty
+func SortByProperty(data []interface{}, key string, nilFirst bool) {
+	values.SortByProperty(data, key, nilFirst)
+}
+
+// ParseDate should be replaced by values.SortByProperty
+func ParseDate(s string) (time.Time, error) {
+	return values.ParseDate(s)
+}
