ae6b280abee818019ad0b92d372a3d701794b48e
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-10-14 16:37:56 +0300

    Add --env for binding environment in the CLI tool

--- cmd/liquid/main_test.go
+++ cmd/liquid/main_test.go
@@ -2,47 +2,112 @@ package main
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestMain(t *testing.T) {
-	exit = func(n int) { t.Fatalf("exit called") }
+	oldArgs := os.Args
+
+	defer func() {
+		os.Args = oldArgs
+		stderr = os.Stderr
+		stdout = os.Stdout
+		stdin = os.Stdin
+		exit = os.Exit
+		env = os.Environ
+		bindings = map[string]interface{}{}
+	}()
+
+	exit = func(n int) {
+		t.Fatalf("exit called")
+	}
+
+	os.Args = []string{"liquid"}
 
 	// stdin
 	src := `{{ "Hello World" | downcase | split: " " | first | append: "!"}}`
-	buf := new(bytes.Buffer)
+	buf := &bytes.Buffer{}
 	stdin = bytes.NewBufferString(src)
 	stdout = buf
-	require.NoError(t, run([]string{}))
+	main()
 	require.Equal(t, "hello!", buf.String())
 
-	// filename
-	buf = new(bytes.Buffer)
-	stdin = bytes.NewBufferString("")
+	// environment binding
+	var envCalled bool
+	env = func() []string {
+		envCalled = true
+		return []string{"TARGET=World"}
+	}
+	src = `Hello, {{ TARGET }}!`
+	// without -e
+	stdin = bytes.NewBufferString(src)
+	buf = &bytes.Buffer{}
 	stdout = buf
-	require.NoError(t, run([]string{"testdata/source.txt"}))
+	os.Args = []string{"liquid"}
+	main()
+	require.False(t, envCalled)
+	require.Equal(t, "Hello, !", buf.String())
+	// with -e
+	stdin = bytes.NewBufferString(src)
+	buf = &bytes.Buffer{}
+	stdout = buf
+	os.Args = []string{"liquid", "--env"}
+	main()
+	require.True(t, envCalled)
+	require.Equal(t, "Hello, World!", buf.String())
+
+	// filename
+	stdin = os.Stdin
+	buf = &bytes.Buffer{}
+	stdout = buf
+	os.Args = []string{"liquid", "testdata/source.txt"}
+	main()
 	require.Contains(t, buf.String(), "file system")
 
-	// missing file
-	require.Error(t, run([]string{"testdata/missing_file"}))
+	// following tests test the exit code
+	var exitCalled bool
+	exitCode := 0
+	exit = func(n int) { exitCalled = true; exitCode = n }
 
+	os.Args = []string{"liquid", "testdata/source.txt"}
+	main()
+	require.Equal(t, 0, exitCode)
+
+	exitCode = 0
+	// missing file
+	buf = &bytes.Buffer{}
+	stderr = buf
+	os.Args = []string{"liquid", "testdata/missing_file"}
+	main()
+	require.Equal(t, 1, exitCode)
+	require.Contains(t, buf.String(), "no such")
+
+	exitCalled = false
 	// --help
-	buf = new(bytes.Buffer)
-	stdout = buf
-	require.NoError(t, run([]string{"--help"}))
+	buf = &bytes.Buffer{}
+	stderr = buf
+	os.Args = []string{"liquid", "--help"}
+	main()
 	require.Contains(t, buf.String(), "usage:")
+	require.True(t, exitCalled)
+	require.Equal(t, 0, exitCode)
 
 	// --undefined-flag
-	exitCode := 0
-	exit = func(n int) { exitCode = n }
-	require.NoError(t, run([]string{"--undefined-flag"}))
+	buf = &bytes.Buffer{}
+	stderr = buf
+	os.Args = []string{"liquid", "--undefined-flag"}
+	main()
 	require.Equal(t, 1, exitCode)
+	require.Contains(t, buf.String(), "defined")
 
 	// multiple args
-	exitCode = 0
-	exit = func(n int) { exitCode = n }
-	require.NoError(t, run([]string{"file1", "file2"}))
+	os.Args = []string{"liquid", "testdata/source.txt", "file2"}
+	buf = &bytes.Buffer{}
+	stderr = buf
+	main()
+	require.Contains(t, buf.String(), "too many")
 	require.Equal(t, 1, exitCode)
 }
